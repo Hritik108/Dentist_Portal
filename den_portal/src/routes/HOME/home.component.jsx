e99d91a79ccefbe9ac269530279ca90eb74d9b07
hritik <hritik.chauhan@restaverse.com>
2025-01-17 12:50:39 +0530

    hello

--- den_portal/src/routes/HOME/home.component.jsx
+++ den_portal/src/routes/HOME/home.component.jsx
@@ -6,7 +6,7 @@ return ( <div>
     {/* <header>
       <nav>
         <ul>
-          <li><a href="#">Home</a></li>
+          <li><a href="#">Homeeii</a></li>
           <li><a href="#">Services</a></li>
           <li><a href="#">About Us</a></li>
           <li><a href="#">Contact Us</a></li>
